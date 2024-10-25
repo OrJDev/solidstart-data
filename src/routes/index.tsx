03105761a041f5c92d2dabdaa4364106d15ff736
OrJDev <orjdeveloper@gmail.com>
2024-10-25 19:34:14 +0300

    with todo-app

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { type VoidComponent } from "solid-js";
+
+const Home: VoidComponent = () => {
+  return (
+    <main class="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#026d56] to-[#152a2c]"></main>
+  );
+};
+
+export default Home;
